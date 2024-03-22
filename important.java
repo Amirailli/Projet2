4180651ab2272077128af286e65aa8e6a93c133a
ibtissemladjici50@gmail.com <ibtissemladjici50@gmail.com>
2024-03-22 16:20:22 +0100

    empthy message

--- important.java
+++ important.java
@@ -8,14 +8,4 @@ package amartt;
  */
 public class important {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
- int j= 0 ;
- j++;
- System.out.println ("hello world !"+ j )
-	}
-
 }
